a8eead211e8e54132deeae2112983b8adb18ec85
Sam Hemingway <samphemingway@icloud.com>
2023-07-07 10:24:15 -0400

    Updated template component

--- src/components/_TEMPLATE/COMPONENT.js
+++ src/components/_TEMPLATE/COMPONENT.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import * as styles from "./COMPONENT.css.ts";
 
 function COMPONENT() {
-  return <div>Here's your component!</div>;
+  return <div>Heres your component!</div>;
   /* Don't forget to change the name of your component in: 
     - This file.
     - In index.js
